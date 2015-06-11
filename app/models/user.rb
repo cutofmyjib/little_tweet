e1f36ff42f80beb647c04b22d2c685dce71410ed
Henry Vega & James Newman <pair+hcode1.jnewman12@devbootcamp.com>
2015-06-11 16:01:10 -0700

    implemented has_many_through relationship, added the migration and relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
+ has_many :tweets
+
+ has_many :followers, :through => :followed_relationships,
+ :source => :following
+ has_many :follow_relationships, :class_name => "Relationship", :foreign_key => "following_id"
+
+ has_many :followed_relationships, :class_name => "Relationship", :foreign_key => "follower_id"
+ has_many :followings, :through => :follow_relationships, :source => :follower
+
+
  validates :user_name, uniqueness: true
  validates :email, uniqueness: true
 end
