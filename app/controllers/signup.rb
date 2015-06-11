e78b4b1d05cc3cdb3e7c27162c1604d84c2c6447
Diana Lozano & Ellis Marte <pair+cutofmyjib.ellismarte@devbootcamp.com>
2015-06-11 13:14:54 -0700

    Added bcrypt and made routes RESTful

--- app/controllers/signup.rb
+++ app/controllers/signup.rb
@@ -1,9 +1,37 @@
 get '/signup' do
+  #  yeild the form to create a new user
   erb :signup
 end
 
 
-post '/logged_in' do
- new_user = User.create!(name: params[:name], email: params[:email], username: params[:username], password: params[:password], password_confirmation: params[:password_confirmation])
+post '/new_user' do
+  # post route to recieve new user information.
+
+  @user = User.new(name: params[:name],
+                   email: params[:email],
+                   user_name: params[:username])
+  @user.password = params[:password]
+  @user.save!
+
   redirect '/'
+end
+
+post '/sessions' do
+  @user = User.find_by(email: params[:email])
+  if @user.password == params[:password]
+    session[:user_id] = @user.id
+  else
+    erb :signin
+  end
+end
+
+delete '/sessions' do
+  # LOGOUT functionality
+  session[:user_id] = nil
+  redirect '/'
+end
+
+get '/' do
+  # if session[user_id]
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+
  validates :user_name, uniqueness: true
  validates :email, uniqueness: true
+
+
+  # users.password_hash in the database is a :string
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
