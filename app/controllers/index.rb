491920da6720b039c7778cd2e32f36e6cf35594d
erictflores & Vivek Poola <pair+erictflores.vpoola88@devbootcamp.com>
2015-06-11 20:56:46 -0700

    finish mvp controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,28 +1,38 @@
 # If a user signs in
 get '/' do
-  redirect '/tweets'
 
+redirect '/tweets'
 
 end
 
 get '/tweets' do
-    @tweets = Tweet.all
+  #p @user = User.where(id: params[:id]).first
+
+
+  if session[:user_id]
+      @user = User.find(session[:user_id])
+      p session
+      @tweets = @user.tweets
+    p "INDEX"
     erb :index
+  else
+    redirect '/signup'
+  end
 end
 
 
 # If a user wants to create a tweet
 
 post '/tweets' do
-  Tweet.create(body: params[:body])
+  Tweet.create(body: params[:body], user_id: session[:user_id])
 
   redirect to '/tweets'
 end
 
 
 get '/tweets/:tweet_id' do
-  @tweet_id = Tweet.find_by(id: params[:tweet_id])
-
+  @tweet_id = Tweet.find_by(id: params[:tweet_id] )
+  @user = User.find(session[:user_id])
   erb :tweet
 end
 
