0cc0d044d4be56d86f679e6be6a0ea63c4235bfc
Vivek Poola <email": null>
2015-06-12 11:49:46 -0700

    dont be on master

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,4 @@
 class Relationship < ActiveRecord::Base
   belongs_to :follower, :class_name => "User", :foreign_key => "follower_id"
-  belongs_to :following, :class_name => "User", :foreign_key => "followed_id"
+  belongs_to :following, :class_name => "User", :foreign_key => "following_id"
 end
-xx
